f161bdbc0f17e2da568c935cd58542eef3dcd0ac
Tom <thaborak@gmail.com>
2018-05-31 20:44:17 -0400

    correct/ incorrect feedback implemented  :sparkles:

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { Field, reduxForm, focus, reset } from 'redux-form';
 import Input from './input';
 import requiresLogin from './requires-login';
-import {fetchProtectedData} from '../actions/protected-data';
+import { fetchProtectedData } from '../actions/protected-data';
 import { QuestionForm } from './question-form';
 import { QuestionCard } from './question-card';
-
+import FooterBar from './footer-bar';
+import HeaderBar from './header-bar';
 
 
 export class Dashboard extends React.Component {
@@ -15,27 +16,29 @@ export class Dashboard extends React.Component {
     }
     render() {
         return (
-            
+
             <div className="dashboard">
+                <HeaderBar />
                 <div className="dashboard-username">
                     Username: {this.props.username}
                 </div>
                 <div className="dashboard-protected-data">
-                    <QuestionCard question={this.props.protectedData} />
+                    <QuestionCard question={this.props.protectedData} response={this.props.response} />
                 </div>
-                
-
+                <FooterBar response={this.props.response} />
             </div>
+
         );
     }
 }
 
 const mapStateToProps = state => {
-    const {currentUser} = state.auth;
+    const { currentUser } = state.auth;
     return {
         username: state.auth.currentUser.username,
         name: `${currentUser.fullname}`,
-        protectedData: state.protectedData.data
+        protectedData: state.protectedData.data,
+        response: state.protectedData.response
     };
 };
 
---
+++ src/components/footer-bar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { clearAuth } from '../actions/auth';
+import { clearAuthToken } from '../local-storage';
+import { Link, Redirect } from 'react-router-dom';
+import './footer-bar.css'
+import sigil from '../assets/img/sigil.jpg';
+
+export class FooterBar extends React.Component {
+    render() {
+        console.log(this.props.response)
+        return <div className="footer-bar">
+        </div>;
+    }
+}
+
+export default connect()(FooterBar);

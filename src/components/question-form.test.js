531c3a637fc277ab86e4b8641455823a07d1954e
biancavee15 <vasquezbmarie@gmail.com>
2018-06-01 13:21:00 -0400

    tests not working

---
+++ src/components/question-form.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import QuestionForm from './question-form';
+
+describe('<QuestionForm />', () => {
+    it('Renders without crashing', () => {
+        shallow(<QuestionForm />);
+    });
+ 
+});
